688c7686a682d27e5f592d7cff567ced8ef972fc
Matthew Howat <matthewj.howat@gmail.com>
2019-02-02 02:32:24 -0700

    working on bringing in the advice api

--- ember_practice_1/app/components/advice-generator.js
+++ ember_practice_1/app/components/advice-generator.js
@@ -1,4 +1,27 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+    fetchAdvice(){
+        fetch('https://api.adviceslip.com/advice')
+        .then(data => data.json()) // see Response.json() in the Fetch API spec
+        .then(
+            (result) => {
+                result = slip.advice
+                // console.log(result);
+                this.setState({
+                    isLoaded: true,
+                    json: result
+                });
+            },
+            (error) => {
+                this.setState({
+                    isLoaded: false,
+                    error
+                });
+            }
+        );
+   }
+
+     
 });
