44526a2c4231c6dd48acd4bebbf23aa951ff0932
Matthew Howat <mhowat1@nait.ca>
2019-02-04 11:15:42 -0700

    adding click handler in ember

--- ember_practice_1/app/components/advice-generator.js
+++ ember_practice_1/app/components/advice-generator.js
@@ -2,13 +2,26 @@ import Component from '@ember/component';
 
 export default Component.extend({
 
+    actions: {
+        fetchJson(event) {
+            try {
+                this.advice = fetchAdvice();
+            }
+            catch {
+                console.log(error);
+            }
+        }
+    },
+
+
+
     fetchAdvice(){
         fetch('https://api.adviceslip.com/advice')
         .then(data => data.json()) // see Response.json() in the Fetch API spec
         .then(
             (result) => {
                 result = slip.advice
-                // console.log(result);
+                console.log(result);
                 this.setState({
                     isLoaded: true,
                     json: result
